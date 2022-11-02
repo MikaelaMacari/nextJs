d5c88d1abaaccbd5d5f7cc873213a3e64bd0cdeb
Mihaela Macari <mikaela.malanici@gmail.com>
2022-11-02 15:20:50 +0200

    added loading component

---
+++ nextjs13-demo/app/photos/loading.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const ImagesLoading = () => {
+  return (
+    <div class="d-flex justify-content-center">
+      <div class="spinner-border text-warning" role="status">
+        <span class="visually-hidden text-warning">Loading...</span>
+      </div>
+    </div>
+  );
+};
+
+export default ImagesLoading;
